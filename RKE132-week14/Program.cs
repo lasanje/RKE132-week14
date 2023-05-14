aeb5c6efea56567de6f79f9cf5b95157723ca7ae
SP <spukk@tktk.ee>
2023-05-14 13:55:14 +0300

    add week14 code

---
+++ RKE132-week14/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Name your dog");
+string myDogName = Console.ReadLine();
+
+Dog mydog = new Dog(myDogName);
+Dog neigouboursdog = new Dog("Good girl");
+Console.WriteLine($"My dog name is {mydog.Name}");
+Console.WriteLine($"My neighbours dog name is {neigouboursdog.Name}");
+
+
+mydog.Rename("Bad Boy");
+while (mydog.LevelOfHapiness != 5)
+{
+    mydog.Bark();
+}
+
+mydog.WagTail();
+
+class Dog
+{
+    private string _name;
+    private int _levelOfHapiness;
+
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has beed renamed to: {newName}");
+    }
+    public void Bark()
+    {
+        Console.WriteLine("woof-woof");
+        _levelOfHapiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("woggle-wiggle");
+    }
+}
+
